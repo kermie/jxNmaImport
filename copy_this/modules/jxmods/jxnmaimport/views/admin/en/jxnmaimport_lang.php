d1f821170e6b2a1d94e7f968c245f3834cbc1b1e
Marco Steinhaeuser <marco.steinhaeuser@gmail.com>
2013-12-21 00:21:17 +0100

    Streamlined English translations
    If possible from the space side, I'd suggest to not use "NA" as an abbreviation for "not available". It might be mixed up with the abbreviation for "North America".

--- copy_this/modules/jxmods/jxnmaimport/views/admin/en/jxnmaimport_lang.php
+++ copy_this/modules/jxmods/jxnmaimport/views/admin/en/jxnmaimport_lang.php
@@ -2,14 +2,14 @@
 $sLangName  = "English";
 $iLangNr    = 1;
 $aLang = array(
-    'jxnmaimport_menu'         => 'Deactivate NA-Products',
+    'jxnmaimport_menu'         => 'Deactivate NA products',
     
-    'JXNMAIMPORT_TITLE'        => 'Deactivate no more available products',
-    'JXNMAIMPORT_DEACTIVATE'   => 'Alle markierten Artikel deaktivieren',
-    'JXNMAIMPORT_MPN'          => 'Herst.-Art.-Nr. (MPN)',
+    'JXNMAIMPORT_TITLE'        => 'Deactivate unavailable products',
+    'JXNMAIMPORT_DEACTIVATE'   => 'Deactivate all marked products',
+    'JXNMAIMPORT_MPN'          => 'Manufacturer product number (MPN)',
 
-    'SHOP_MODULE_GROUP_JXNMAIMPORT_IMPORT'            => 'Import Settings',
-    'SHOP_MODULE_sJxNmaImportDelimeter'               => 'CSV Delimeter',
+    'SHOP_MODULE_GROUP_JXNMAIMPORT_IMPORT'            => 'Import settings',
+    'SHOP_MODULE_sJxNmaImportDelimeter'               => 'CSV delimiter',
     'SHOP_MODULE_sJxNmaImportDelimeter_comma'         => ',',
     'SHOP_MODULE_sJxNmaImportDelimeter_semicolon'     => ';',
     'SHOP_MODULE_sJxNmaImportDelimeter_tabulator'     => 'Tab',
