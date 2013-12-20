18174f8f5025e61674bae2cd42c6dc8ed64907f8
Marco Steinhaeuser <marco.steinhaeuser@gmail.com>
2013-12-21 00:17:56 +0100

    streamlined English translations
    If possible from the space side, I'd suggest to not use "NA" as an abbreviation for "not available". It might be mixed up with the abbreviation for "North America".

--- copy_this/modules/jxmods/jxnmaimport/views/admin/en/jxnmaimport_lang.php
+++ copy_this/modules/jxmods/jxnmaimport/views/admin/en/jxnmaimport_lang.php
@@ -9,7 +9,7 @@ $aLang = array(
     'JXNMAIMPORT_MPN'          => 'Herst.-Art.-Nr. (MPN)',
 
     'SHOP_MODULE_GROUP_JXNMAIMPORT_IMPORT'            => 'Import Settings',
-    'SHOP_MODULE_sJxNmaImportDelimeter'               => 'CSV Delimeter',
+    'SHOP_MODULE_sJxNmaImportDelimeter'               => 'CSV delimeter',
     'SHOP_MODULE_sJxNmaImportDelimeter_comma'         => ',',
     'SHOP_MODULE_sJxNmaImportDelimeter_semicolon'     => ';',
     'SHOP_MODULE_sJxNmaImportDelimeter_tabulator'     => 'Tab',
